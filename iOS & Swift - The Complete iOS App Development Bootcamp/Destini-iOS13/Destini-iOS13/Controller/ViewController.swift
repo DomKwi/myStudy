d3452476c070a2f8d58b62410fef46d2a06c5ce8
Dominik <domkwi18@gmail.com>
2024-08-18 18:41:35 +0200

    implementing the mvc pattern
    implementing patern mvc into the project
    - transferring example structure to story brain
    - creating text story method
    - fixing errors in viewdidload
    - fixing a bug in updateUI

--- iOS & Swift - The Complete iOS App Development Bootcamp/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var storyBrain = StoryBrain()
     
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
@@ -16,41 +17,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = example[0].title
-        choice1Button.setTitle(example[0].choice1, for: .normal)
-        choice2Button.setTitle(example[0].choice2, for: .normal)
+        storyLabel.text = storyBrain.example[0].title
+        choice1Button.setTitle(storyBrain.example[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.example[0].choice2, for: .normal)
     }
-    var storyNumber = 0
+    
     
     @IBAction func choiceMade(_ sender: UIButton) {
-        let currentStory = example[storyNumber]
         let userChoice = sender.currentTitle!
         print(sender.currentTitle!)
         
-        if userChoice == currentStory.choice1 {
-            storyNumber = 1
-        } else if userChoice == currentStory.choice2 {
-            storyNumber = 2
-        }
         UpdateUI()
     }
     
-    let example = [
-        Story(title: "You see a fork in the road.",
-              choice1: "Take a left",
-              choice2: "Take a right"),
-        Story(title: "You see a tiger",
-              choice1: "shout for help",
-              choice2: "play dead"),
-        Story(title: "You find a treasure chest",
-              choice1: "Open it.",
-              choice2: "check for traps")
-    ]
+    
     
     func UpdateUI() {
-        storyLabel.text = example[storyNumber].title
-        choice1Button.setTitle(example[storyNumber].choice1, for: .normal)
-        choice2Button.setTitle(example[storyNumber].choice2, for: .normal)
+        storyLabel.text = storyBrain.example[storyBrain.storyNumber].title
+        choice1Button.setTitle(storyBrain.example[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.example[storyBrain.storyNumber].choice2, for: .normal)
     }
 }
 
--- iOS & Swift - The Complete iOS App Development Bootcamp/Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
@@ -9,5 +9,27 @@
 import Foundation
 
 struct StoryBrain {
+    var storyNumber = 0
     
+    let example = [
+        Story(title: "You see a fork in the road.",
+              choice1: "Take a left",
+              choice2: "Take a right"),
+        Story(title: "You see a tiger",
+              choice1: "shout for help",
+              choice2: "play dead"),
+        Story(title: "You find a treasure chest",
+              choice1: "Open it.",
+              choice2: "check for traps")
+    ]
+    
+    mutating func nextStory(userChoice: String) {
+        let currentStory = example[storyNumber]
+        
+        if userChoice == currentStory.choice1 {
+            storyNumber = 1
+        } else if userChoice == currentStory.choice2 {
+            storyNumber = 2
+        }
+    }
 }

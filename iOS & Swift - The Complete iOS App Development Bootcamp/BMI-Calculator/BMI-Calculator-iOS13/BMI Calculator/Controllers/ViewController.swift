d3378adfc78cb4e9c4678732285b45dc05df2371
Dominik <domkwi18@gmail.com>
2024-09-02 17:03:09 +0200

    addition of bmi calculator

--- iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -11,21 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func heightSlider(_ sender: UISlider) {
-        print(sender.value)
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
         let heightValue = sender.value
         heightLabel.text = String(format: "%.2f", heightValue) + "m"
     }
     
-    @IBAction func weightSlider(_ sender: UISlider) {
-        print(sender.value)
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
         let weightValue = sender.value
-        weightLabel.text = String(format: "%.0f", weightValue) + "kg"
+        weightLabel.text = String(format: "%.0f", weightValue) + "Kg"
+    }
+    
+    @IBAction func calculatedPressed(_ sender: Any) {
+        let heightValue = heightSlider.value
+        let weightValue = weightSlider.value
+        let bmi = weightValue / (heightValue * heightValue)
+        let result = String(format: "%.1f", bmi)
+        print(result)
+
     }
     
 }

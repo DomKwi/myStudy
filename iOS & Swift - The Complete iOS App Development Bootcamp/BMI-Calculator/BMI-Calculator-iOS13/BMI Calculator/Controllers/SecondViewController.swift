5cae18c86f3a90ccc3d101258597a0004eaae8ef
Dominik <domkwi18@gmail.com>
2024-09-07 17:37:49 +0200

    file preparation
    Prepare a work file for the next lesson and discuss the differences between classes and structures

--- iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -5,5 +5,11 @@
 //  Created by Dominik Kwiecień on 07/09/2024.
 //  Copyright © 2024 Angela Yu. All rights reserved.
 //
-import Foundation
+import UIKit
 
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

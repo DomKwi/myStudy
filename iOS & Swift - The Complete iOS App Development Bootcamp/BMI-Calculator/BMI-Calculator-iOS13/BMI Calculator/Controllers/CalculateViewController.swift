8a51d76aaf4d7012c1e58704d26fafc622c20851
Dominik <domkwi18@gmail.com>
2024-09-12 00:48:25 +0200

    broken commit

--- iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
+    var bmiValue: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         let heightValue = sender.value
         heightLabel.text = String(format: "%.2f", heightValue) + "m"
@@ -32,18 +35,21 @@ class ViewController: UIViewController {
         let heightValue = heightSlider.value
         let weightValue = weightSlider.value
         let bmi = weightValue / (heightValue * heightValue)
+        bmiValue = bmi
        
+        print(bmi)
         //adding a view using the segue identifier goToResult
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
     
     //preparation for the segue, if the segue is the same as our path to the resultviewcontroller then pass the data bmi
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController
-            destinationVC.bmiValue = "0.0"
+            destinationVC.bmiValue = String(format: "%.1f", bmiValue)
             
-            //The creation of a distinction on the path from the button is sent data to the function is made to distinguish where they are to go. Then pass the data to the result view and assign the value to bmiValue 
+            //The creation of a distinction on the path from the button is sent data to the function is made to distinguish where they are to go. Then pass the data to the result view and assign the value to bmiValue
         }
     }
 }

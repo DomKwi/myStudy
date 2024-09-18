146d8055a34163958862637124304e8dbca99dd1
Dominik <domkwi18@gmail.com>
2024-09-18 14:00:18 +0200

    implementation of the getBMIVAlue function

--- iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
@@ -10,8 +10,15 @@ import Foundation
 
 struct CalculatorBrain {
     
-    func calculateBMI(height: Float, weight: Float) {
-        let bmi = weight / (height * height)
+    var bmi: Float = 0.0
     
+    mutating func calculateBMI(height: Float, weight: Float) {
+        bmi = weight / (height * height)
+    
+    }
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        return bmiTo1DecimalPlace
     }
 }

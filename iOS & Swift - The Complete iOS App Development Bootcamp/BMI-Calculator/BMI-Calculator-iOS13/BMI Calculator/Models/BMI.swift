aae59852c4b8f0117f29bfbddd7badaed7cbaa0f
Dominik <domkwi18@gmail.com>
2024-09-20 10:53:47 +0200

    creating a bmi model and performing the task
    performing the task of creating if conditions for the bmi calculator

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Dominik Kwiecień on 20/09/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: Color
+}
--- iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator/BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
@@ -7,19 +7,29 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            print("You are underweight")
+        } else if bmiValue < 24.9 {
+            print("You are healthy")
+        } else {
+            print("You are overweight")
+        }
+        bmi = BMI(value: bmiValue, advice: <#T##String#>, color: <#T##Color#>)
     
     }
     
     func getBMIValue() -> String {
         
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
     }
 }

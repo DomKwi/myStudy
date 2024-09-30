f9ad272ce5ea719e4740eb3e411a6b9734514794
Dominik <domkwi18@gmail.com>
2024-09-30 11:34:02 +0200

    plugging the view into a new coca class

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Tipsy/Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Dominik Kwiecień on 30/09/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

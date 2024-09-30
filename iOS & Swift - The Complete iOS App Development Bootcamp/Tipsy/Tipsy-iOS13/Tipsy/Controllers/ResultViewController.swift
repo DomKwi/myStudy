cd889a4cccbd66e3ef331b951613bf4383fcadcd
Dominik <domkwi18@gmail.com>
2024-09-30 11:53:00 +0200

    pinning ui elements

--- iOS & Swift - The Complete iOS App Development Bootcamp/Tipsy/Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Tipsy/Tipsy-iOS13/Tipsy/Controllers/ResultViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }

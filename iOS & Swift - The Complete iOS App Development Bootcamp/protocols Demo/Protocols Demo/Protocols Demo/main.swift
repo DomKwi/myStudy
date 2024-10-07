46307da62dd8bc2c1b775a709d8abc069f0c678e
Dominik <domkwi18@gmail.com>
2024-10-07 13:00:40 +0200

    demonstration of the functionality of the protocols
    Illustrating the functionalities and applications of protocols through practical examples.

--- iOS & Swift - The Complete iOS App Development Bootcamp/protocols Demo/Protocols Demo/Protocols Demo/main.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/protocols Demo/Protocols Demo/Protocols Demo/main.swift
@@ -1,3 +1,6 @@
+protocol CanFly {
+    func fly()
+}
 
 class Bird {
     
@@ -8,15 +11,40 @@ class Bird {
             print("The bird makes a new bird in a shell.")
         }
     }
-    
+}
+
+class Eagle: Bird, CanFly {
     func fly() {
-        print("The bird flasps its wings and liff in to the sky.")
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
     }
 }
 
-class Eagle: Bird {
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct flyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lif off into the air.")
+    }
 }
 
 let myEagle = Eagle()
-myEagle.layEggs()
 myEagle.fly()
+myEagle.layEggs()
+let myPenguin = Penguin()
+myPenguin.layEggs()
+let myPlane = Airplane()
+myPlane.fly()

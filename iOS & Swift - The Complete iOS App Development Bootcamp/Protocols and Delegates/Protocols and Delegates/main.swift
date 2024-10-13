094e4ed33ec94ee34e090563cc3e92f1e3037fd2
Dominik <domkwi18@gmail.com>
2024-10-13 20:11:44 +0200

    another example
    Another example of how, without altering the functions listed below in the code, you can alter the program’s functionality.

--- iOS & Swift - The Complete iOS App Development Bootcamp/Protocols and Delegates/Protocols and Delegates/main.swift
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Protocols and Delegates/Protocols and Delegates/main.swift
@@ -27,8 +27,36 @@ struct Paramedic: AdvanceLifeSupport {
 }
 
 
+class Doctor: AdvanceLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per secend.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds")
+    }
+    
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    
+    func useElectronicDrill() {
+        print("Whirr...")
+    }
+}
+
+
 let emilio = EmergencyCallHandler()
-let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
 
 emilio.assessSituation()
 emilio.medicalEmergency()
